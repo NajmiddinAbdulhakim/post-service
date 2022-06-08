cfb274f8cc6ecf0bcaedcf668af2f7aeb45961af
Najmiddin <najmiddin@example.com>
2022-06-08 10:19:21 +0500

    3

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR=$(pwd)
 
 protoc -I /usr/local/include \
-       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $GOPATH/pkg/mod/google.golang.org/protobuf@v1.28.0/proto \
        -I $CURRENT_DIR/protos/ \
         --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/ \
         $CURRENT_DIR/protos/*.proto;
